afb7fdfed279cbc8f1c892ecc92105ab357ee966
ageorge-blip <ageorge@mail.endicott.edu>
2025-11-13 14:59:57 -0500

    Update date in biosaple-filter.py header

--- biosaple-filter.py
+++ biosaple-filter.py
@@ -1,6 +1,6 @@
 """
 Author: Alicia George
-Date: October 13, 2025
+Date: November 13, 2025
 Description:
     This script retrieves metadata from the NCBI BioSample database for a list of BioSample IDs,
     extracts selected survey/metadata fields, and exports a filtered CSV file.
